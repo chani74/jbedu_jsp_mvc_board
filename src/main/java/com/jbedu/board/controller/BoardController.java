5fc1ac604e2c8e2627e7703327c7d078d05cb546
chani74 <chani74@gmail.com>
2024-12-12 16:22:47 +0900

    commit

---
+++ src/main/java/com/jbedu/board/controller/BoardController.java
@@ -0,0 +1,50 @@
+package com.jbedu.board.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")  //컨트롤러 기능의 클래스로 서버가 인식하도록 함
+public class BoardController extends HttpServlet {
+
+	public BoardController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet호출");
+		
+		request.setCharacterEncoding("utf-8");
+		
+		String viewPage = "list.jsp" ;	//글 목록 페이지
+			
+		String uri = request.getRequestURI()	;
+		String conPath = request.getContextPath();  
+		String com = uri.substring(conPath.length());
+
+		System.out.println(uri);
+		System.out.println(conPath);
+		System.out.println(com);
+		
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request,response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+
+		
+}
