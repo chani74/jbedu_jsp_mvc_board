132cdea9aa32c1dc38e10461c38f7dbed1840d5d
chani74 <chani74@gmail.com>
2024-12-13 09:07:08 +0900

    commit

--- src/main/java/com/jbedu/board/controller/BoardController.java
+++ src/main/java/com/jbedu/board/controller/BoardController.java
@@ -42,7 +42,7 @@ public class BoardController extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+
 	}
 
 
